216e12fded5c35f05a791334ae5acef81580920f
NSPreetham <50166305+NSPreetham@users.noreply.github.com>
2019-07-13 11:40:47 +1000

    Update JFun.cs

--- JealFunctions/JFun.cs
+++ JealFunctions/JFun.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,43 +14,68 @@ namespace JealTest
         public static string CapitalNames(string str)
         {
             string result = str;
+            var reg = new Regex("^[a-zA-Z]+$");
             if (!string.IsNullOrEmpty(str))
             {
                 var words = str.Split(' ');
                 for (int index = 0; index < words.Length; index++)
                 {
                     var s = words[index];
-                    if (s.Length > 0)
+                    
+                    if (reg.IsMatch(s))
                     {
-                        words[index] = s[0].ToString().ToUpper() + s.Substring(1).ToLower();
+                       words[index] = s[0].ToString().ToUpper() + s.Substring(1).ToLower();
+                       result = string.Join(" ", words);
                     }
-                }
-                result = string.Join(" ", words);
-                
+                    else
+                    {
+                        Console.WriteLine("Only Strings Allowed in Names");
+                        result = "";
+                        break;                       
+                    }                    
+                }                             
+              
             }
             return result;
         }
 
         public static string ReducedFraction(string input)
         {
+            Regex regex = new Regex(@"[\d]");
             string[] i = input.Split('/');
 
-            int Operand1 = Convert.ToInt32(i[0]);
-            int Operand2 = Convert.ToInt32(i[1]);
-            int Numerator = Operand1;
-            int Denominator = Operand2;
-            int divisor = gcd(Operand1, Operand2);
-
-            if (divisor != 1)
+            try
             {
-                Numerator = division(Operand1, divisor);
-                Denominator = division(Operand2, divisor);            }
+                int Operand1 = Convert.ToInt32(i[0]);
+                int Operand2 = Convert.ToInt32(i[1]);
+                if (regex.IsMatch(i[0]) && regex.IsMatch(i[1]) && (Operand1 !=0 && Operand2 !=0))
+                {
+                    int Numerator = Operand1;
+                    int Denominator = Operand2;
+                    int divisor = gcd(Operand1, Operand2);
 
-            return Denominator == 1 ? Numerator.ToString() : Numerator + "/" + Denominator;
-        }
+                    if (divisor != 1)
+                    {
+                        Numerator = division(Operand1, divisor);
+                        Denominator = division(Operand2, divisor);
+                    }
 
- 
+                    return Denominator == 1 ? Numerator.ToString() : Numerator + "/" + Denominator;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                    return "";
+                }
+            }
 
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid Input");
+                return "";
+            }
+
+        } 
         static int gcd(int a, int b)
         {
             //gcd using Euclid’s algorithm
