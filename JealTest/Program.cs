ed7b138802b5c20f86e42b1ddb6cdeefc5166ba6
NSPreetham <50166305+NSPreetham@users.noreply.github.com>
2019-07-13 11:36:50 +1000

    Update Program.cs

--- JealTest/Program.cs
+++ JealTest/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using JealTest;
 
 
@@ -7,12 +7,44 @@ namespace JealTest
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine(JFun.CapitalNames("preetam gautam uday"));                             
+        {
+            try
+            {
+                Console.WriteLine("Please Enter 1 for Fun with Names and 2 for Fun with fractions");
+                int ch = Convert.ToInt16(Console.ReadLine());
 
-            Console.WriteLine(JFun.ReducedFraction("16/48"));
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("Please Enter Names separated by Space");
+                        string str = Console.ReadLine();
+                        //Console.WriteLine("Hey! I think Names are Fun ");
+                        Console.WriteLine(JFun.CapitalNames(str));
+                        break;
+                    case 2:
+                        Console.WriteLine("Please Enter the fraction");
+                        string strfrac = Console.ReadLine();
+                        // Console.WriteLine("Normalized form of fraction");
+                        Console.WriteLine(JFun.ReducedFraction(strfrac));
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid format");
+                        break;
+                }
 
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine("Enter a Valid Choice");
+            }
         }
+
+
+
+
+    
+        
     }
 }
